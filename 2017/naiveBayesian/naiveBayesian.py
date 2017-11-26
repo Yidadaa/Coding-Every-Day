839fa5561d03b97575989eecb03245351c31f30e
Yidadaa <1143922395@qq.com>
2017-11-26 20:33:08 +0800

    精简不必要的代码和文件

--- 2017/naiveBayesian/naiveBayesian.py
+++ 2017/naiveBayesian/naiveBayesian.py
@@ -4,59 +4,6 @@
 import re
 import numpy as np
 
-class naiveBayesianTextClassifier():
-    def __init__(self, train_data):
-        self.spam_words = self.count(train_data['spam']) # 分别统计垃圾短信和正常短信中的词频
-        self.ham_words = self.count(train_data['ham'])
-        self.common_words = self.count(train_data['spam'] + train_data['ham'])
-        spam_count = len(train_data['spam'])
-        ham_count = len(train_data['ham'])
-        self.spam_prop = spam_count / (spam_count + ham_count) # 计算垃圾短信的概率
-        self.ham_prop = ham_count / (spam_count + ham_count) # 计算正常短信的概率
-        self.train_data = train_data
-        self.total_count = spam_count + ham_count
-
-    def count(self, array_of_text):
-        '''
-        统计单词词频，建立字典
-        '''
-        text_dict = {}
-        total_count = len(array_of_text)
-        for text in array_of_text:
-            words = text.split(' ') # 进行分词
-            words = map(lambda x: re.sub(r'\W', '', x).lower(), words) # 对每一个单词全部取小写，并且祛除其中的标点
-            words = set(words) # 单词去重
-            for word in words: # 统计每个单词出现的次数
-                if len(word) == 0:
-                    continue
-                if word in text_dict.keys():
-                    text_dict[word] += 1
-                else:
-                    text_dict[word] = 2 # 这里进行拉普拉斯平滑
-        for word in text_dict:
-            text_dict[word] /= total_count + 2 # 计算频率，同样对分母做平滑处理
-        return text_dict
-
-    def predict(self, text):
-        '''
-        进行文本分类
-        '''
-        words = text.split(' ') # 进行分词
-        words = map(lambda x: re.sub(r'\W', '', x).lower(), words) # 对每一个单词全部取小写，并且祛除其中的标点
-        words = set(words) # 单词去重
-        a = self.spam_prop
-        b = self.ham_prop
-        for word in words:
-            if word in self.common_words:
-                # self.common_words[word]
-                a *= self.spam_words[word] if word in self.spam_words else 1 / (self.total_count + 2)
-                b *= self.ham_words[word] if word in self.ham_words else 1 / (self.total_count + 2)
-        
-        is_spam = a / (a + b) # 为了便于判断，对a和b进行一些处理
-        is_ham = b / (a + b)
-
-        return { 'spam': is_spam, 'ham': is_ham }
-
 class NBClassifier():
     def __init__(self):
         self.groups_prob = {}
