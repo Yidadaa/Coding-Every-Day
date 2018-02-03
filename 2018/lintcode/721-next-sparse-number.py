c70b5d5da30fd82b5732d92e9e9d488b9826a39c
Yidadaa <1143922395@qq.com>
2018-02-04 00:01:17 +0800

    fix: AC lintcode NO.721

--- 2018/lintcode/721-next-sparse-number.py
+++ 2018/lintcode/721-next-sparse-number.py
@@ -1,3 +1,7 @@
+"""
+lintcode有毒，同样一段代码，C++可以过，python就TLE，shit
+"""
+
 class Solution:
     """
     @param: : a number
@@ -5,25 +9,12 @@ class Solution:
     """
 
     def nextSparseNum(self, x):
-        """
-        判断是否是稀疏数
-        """
-        def isSparseNum(n):
-            last = False
-            while n != 0:
-                cur = ((n >> 1) & 1)
-                if cur and last:
-                    return False
-                last = cur
-                n = n >> 1
-            return True
-        
-        while not isSparseNum(x):
+        while (x >> 1) & x:
             x += 1
         return x
 
 if __name__ == '__main__':
     testClass = Solution()
-    testCase = [6, 4, 38, 0, -1, 341381939]
+    testCase = [6, 4, 38, 0, -1, 341381939, 372228240]
     res = map(testClass.nextSparseNum, testCase)
     print(list(res))
