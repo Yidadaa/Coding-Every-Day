4a9a2c2ae4ba164c7aa474876c40c87ca0b49202
Yidadaa <yidadaa@qq.com>
2019-10-11 00:47:45 +0800

    feat(acwing): change 109 genius acm

--- 2019/acwing/109-genius-acm[CH0601].cpp
+++ 2019/acwing/109-genius-acm[CH0601].cpp
@@ -8,25 +8,34 @@ int tmp[500010];
 int n, m;
 long long t;
 
+int merge_sort(int l, int m, int r) {
+
+}
+
 int solve() {
   int cnt = 0;
   for (int l = 0, r = 0; l < n; l = r) {
-    int step = 1;
+    int step = 1, should_sort = 1;
     while (step > 0) {
       long long sum = 0;
       if (r + step <= n) {
-        for (int i = l; i < r + step; i ++) tmp[i] = a[i];
-        sort(a + l, a + r + step);
+        if (should_sort) {
+          for (int i = l; i < r + step; i ++) tmp[i] = a[i];
+          sort(a + l, a + r + step);
+        }
 
         int i = l, j = r + step - 1;
         for (int t = 0; t < m && i < j; t ++, i ++, j --) {
           sum += (a[i] - a[j]) * (a[i] - a[j]);
         }
 
-        for (int i = l; i < r + step; i ++) a[i] = tmp[i];
+        if (sum <= t) // 如果需要扩展，则将数组复原
+          for (int i = l; i < r + step; i ++) a[i] = tmp[i];
+        else // 否则只复原新增部分数组
+          for (int i = r + step / 2; i < r + step; i ++) a[i] = tmp[i];
       }
-      if (r + step <=n && sum <= t) r += step, step *= 2;
-      else step /= 2;
+      if (r + step <=n && sum <= t) r += step, step *= 2, should_sort = 1;
+      else step /= 2, should_sort = 0;
     }
     cnt ++;
   }
